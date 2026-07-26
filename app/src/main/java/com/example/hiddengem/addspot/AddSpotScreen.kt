405a15e9cbdc3453915f12ffc80b2f05f3bb5bbc
yelo-orens <23103763@usc.edu.ph>
2026-07-26 22:29:38 +0800

    test justine

--- app/src/main/java/com/example/hiddengem/addspot/AddSpotScreen.kt
+++ app/src/main/java/com/example/hiddengem/addspot/AddSpotScreen.kt
@@ -17,7 +17,7 @@ import coil.compose.AsyncImage
 import com.example.hiddengem.map.PickedLocation
 import com.example.hiddengem.util.LightTimes
 
-
+//test justine commit
 @Composable
 fun AddSpotScreen(navController: NavController, vm: AddSpotViewModel = viewModel()) {
     var title by remember { mutableStateOf("") }
