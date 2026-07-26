cddc53bb15f91a4c88efdee4ecf25fba18c98543
HairGelo <shinken1008@gmail.com>
2026-07-26 21:13:16 +0800

    Add AppTextField component

--- app/src/main/java/com/example/hiddengem/components/AppTextField.kt
+++ app/src/main/java/com/example/hiddengem/components/AppTextField.kt
@@ -1,4 +1,27 @@
 package com.example.hiddengem.components
 
-class AppTextField {
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+
+@Composable
+fun AppTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    modifier: Modifier = Modifier,
+    isPassword: Boolean = false
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        singleLine = true,
+        visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
+        modifier = modifier.fillMaxWidth()
+    )
 }
